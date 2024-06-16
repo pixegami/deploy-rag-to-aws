c29d20b0211fcb22f649c7bc43784cdb0b516b64
Jack (Pixegami) <pixegami@gmail.com>
2024-06-16 18:55:14 +1000

    feat: add route53 domain to make API sharable

--- rag-cdk-infra/lib/rag-cdk-infra-stack.ts
+++ rag-cdk-infra/lib/rag-cdk-infra-stack.ts
@@ -5,10 +5,20 @@ import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import {
   DockerImageFunction,
   DockerImageCode,
-  FunctionUrlAuthType,
   Architecture,
 } from "aws-cdk-lib/aws-lambda";
 import { ManagedPolicy } from "aws-cdk-lib/aws-iam";
+import {
+  BasePathMapping,
+  DomainName,
+  LambdaRestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
+import {
+  Certificate,
+  CertificateValidation,
+} from "aws-cdk-lib/aws-certificatemanager";
+import { ApiGatewayDomain } from "aws-cdk-lib/aws-route53-targets";
 
 export class RagCdkInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -63,11 +73,6 @@ export class RagCdkInfraStack extends cdk.Stack {
       },
     });
 
-    // Public URL for the API function.
-    const functionUrl = apiFunction.addFunctionUrl({
-      authType: FunctionUrlAuthType.NONE,
-    });
-
     // Grant permissions for all resources to work together.
     ragQueryTable.grantReadWriteData(workerFunction);
     ragQueryTable.grantReadWriteData(apiFunction);
@@ -76,9 +81,48 @@ export class RagCdkInfraStack extends cdk.Stack {
       ManagedPolicy.fromAwsManagedPolicyName("AmazonBedrockFullAccess")
     );
 
+    // Create a domain API
+    // Create API Gateway
+    const api = new LambdaRestApi(this, "MyApi", {
+      handler: apiFunction,
+    });
+
+    // Specify your domain name
+    const domainName = "api.rag.pixegami.io";
+
+    // Get the hosted zone by domain name
+    const hostedZone = HostedZone.fromLookup(this, "HostedZone", {
+      domainName: "pixegami.io",
+    });
+
+    // Create a certificate for the domain name
+    const certificate = new Certificate(this, "Certificate", {
+      domainName: domainName,
+      validation: CertificateValidation.fromDns(hostedZone),
+    });
+
+    // Create a custom domain for the API Gateway
+    const customDomain = new DomainName(this, "CustomDomain", {
+      domainName: domainName,
+      certificate: certificate,
+    });
+
+    // Map the custom domain to the API Gateway
+    new BasePathMapping(this, "BasePathMapping", {
+      domainName: customDomain,
+      restApi: api,
+    });
+
+    // Create a Route53 record to point to the API Gateway custom domain
+    new ARecord(this, "ApiAliasRecord", {
+      zone: hostedZone,
+      target: RecordTarget.fromAlias(new ApiGatewayDomain(customDomain)),
+      recordName: domainName,
+    });
+
     // Output the URL for the API function.
     new cdk.CfnOutput(this, "FunctionUrl", {
-      value: functionUrl.url,
+      value: api.url,
     });
   }
 }
