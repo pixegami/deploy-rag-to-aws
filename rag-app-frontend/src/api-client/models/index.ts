ee7f4b8893cc662dd095aa3e0ef79852869e4d3d
Jack (Pixegami) <pixegami@gmail.com>
2024-06-16 18:01:17 +1000

    chore: commit generated API client (for site builders)
    Note: Normally we don't want to have to commit this. It's be better if we can just re-generate it at build time.

---
+++ rag-app-frontend/src/api-client/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './HTTPValidationError';
+export * from './QueryModel';
+export * from './SubmitQueryRequest';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
